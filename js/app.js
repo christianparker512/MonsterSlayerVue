5b8e7b6df5b1e9b96885767e1d5ce1c631f64b46
Christian Parker <christianparker512@gmail.com>
2021-06-15 15:56:39 -0500

    play by play with the battle log

--- js/app.js
+++ js/app.js
@@ -8,19 +8,20 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
-      winner: null
+      winner: null,
+      logMessages: []
     };
   },
   computed: {
     monsterBarStyles() {
-      if(this.monsterHealth<0){
-        return {width: '0%'};
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
       }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
-      if(this.playerHealth<0) {
-        return {width: '0%'};
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
       }
       return { width: this.playerHealth + '%' };
     },
@@ -46,30 +47,33 @@ const app = Vue.createApp({
         // Monster lost
         this.winner = 'player';
       }
-    }
+    },
   },
   methods: {
-    startGame(){
-      this.playerHealth= 100;
-      this.monsterHealth =100;
-      this.winner=null;
-      this.currentRound=0;
-      this
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+      this.logMessages = [];
     },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -80,10 +84,18 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('player', 'heal', healValue);
       this.attackPlayer();
     },
-    surrender(){
-      this.winner='monster';
+    surrender() {
+      this.winner = 'monster';
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value
+      });
     }
   },
 });
