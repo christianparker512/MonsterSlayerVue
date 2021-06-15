cfe00ca4044148e60c8f31b4ef1f7fa006afdf10
Christian Parker <christianparker512@gmail.com>
2021-06-15 12:30:28 -0500

    monster and player health when in attack mode

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
+function  getRandomValue(min, max){
+  return Math.floor(Math.random() * (max-min)) +min;
+}
 const app = vue.createApp({
-  data(){}
+  data(){
+    return {
+      playerHealth: 100,
+      monsterHealth: 100
+    };
+  },
+  methods: {
+    attackMonster(){
+      const attackValue = Math.floor(Math.random() * (12 - 5)) +5;
+      this.monsterHealth = this.monsterHealth - attackValue;
+      // this.monsterHealth -= attackValue;
+    },
+    attackPlayer(){
+      const attackValue = Math.floor(Math.random() * (15 - 8)) +8;
+      this.monsterPlayer -= attackValue;
+    }
+  }
 });
 app.mount('#game');
